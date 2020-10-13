8b2324bca5ae34f2e34891e2b3c9154f5ec7bc42
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-10-13 07:19:40 +0700

    remove unused js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Buefy from 'buefy'
 import '@fortawesome/fontawesome-free/js/all'
 import './mystyles.scss'
-import './app.js'
 
 Vue.use(Buefy, {
   defaultIconPack: 'fas'
